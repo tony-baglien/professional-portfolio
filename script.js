5df8601cc44cf70e0475e3767feaec84e82b2b35
TOny Baglien <tony.b.composition@gmail.com>
2021-06-17 11:42:31 -0500

    added media queries

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const about = document.getElementById('about')
-const contact = document.getElementById('contact')
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
+const closers = document.querySelectorAll('#close');
 
-const aboutInfo = document.getElementById('about-info')
-const contactInfo = document.getElementById('contact-info')
-
+const aboutInfo = document.getElementById('about-info');
+const contactInfo = document.getElementById('contact-info');
 
 about.addEventListener('click', () => {
-    contactInfo.classList.add('hidden');
-    if (aboutInfo.classList.contains('hidden')) {
-        aboutInfo.classList.remove('hidden');
-    } else {
-        aboutInfo.classList.add('hidden')
-    }
-})
+  contactInfo.classList.add('hidden');
+  if (aboutInfo.classList.contains('hidden')) {
+    aboutInfo.classList.remove('hidden');
+  } else {
+    aboutInfo.classList.add('hidden');
+  }
+});
 
 contact.addEventListener('click', () => {
-    aboutInfo.classList.add('hidden');
-    if (contactInfo.classList.contains('hidden')) {
-        contactInfo.classList.remove('hidden');
-    } else {
-        contactInfo.classList.add('hidden')
-    }
-})
+  aboutInfo.classList.add('hidden');
+  if (contactInfo.classList.contains('hidden')) {
+    contactInfo.classList.remove('hidden');
+  } else {
+    contactInfo.classList.add('hidden');
+  }
+});
 
+closers.forEach((close) => {
+  close.addEventListener('click', () => {
+    if (!aboutInfo.classList.contains('hidden')) {
+      aboutInfo.classList.add('hidden');
+    } else if (!contactInfo.classList.contains('hidden')) {
+      contactInfo.classList.add('hidden');
+    }
+  });
+});
